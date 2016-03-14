3405196e6ed95211cbdfd61f75b8b12f01fa9808
Muhammad Anas Rashid <anasrasheed22@gmail.com>
2016-03-14 17:58:57 +0500

    Wifi Messenger App

---
+++ app/src/main/java/com/smd/aaapc/wifimessenger/ChatsCallsDevices.java
@@ -0,0 +1,14 @@
+package com.smd.aaapc.wifimessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ChatsCallsDevices extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats_calls_devices);
+    }
+}
